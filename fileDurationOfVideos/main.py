31484716db5cfe4ea2b4e2227cb4b125c7ee2c13
nibivid <nibinv23@gmail.com>
2020-08-13 12:03:22 +0530

    Create main.py

---
+++ fileDurationOfVideos/main.py
@@ -0,0 +1,3 @@
+# "E:\Torrent\AI\Machine Learning Projects A-Z  Kaggle and Real World Pro\Machine Learning Projects A-Z  Kaggle and Real World Pro"
+video=VideoDuration("E:\Torrent\AI\Machine Learning Projects A-Z  Kaggle and Real World Pro\Machine Learning Projects A-Z  Kaggle and Real World Pro")
+video.call()
